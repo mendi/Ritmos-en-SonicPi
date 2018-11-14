c5f6e528def1d03df732823258db5f83c81fc8aa
mendi <vania.unal@gmai.com>
2018-11-14 18:10:41 -0500

    Primeros archivos
    Se crearon 2 archivos: uno para dejarlo quieto, la base del reggaetón y
    uno segundo para mover y realizar pruebas.

---
+++ reggaeton.rb
@@ -0,0 +1,21 @@
+
+
+use_bpm 77
+
+live_loop :kick do
+  sample :bd_haus
+  sleep 1
+end
+
+live_loop :snare do
+  sleep 0.75
+  sample :drum_snare_hard
+  sleep 0.75
+  sample :drum_snare_hard
+  sleep 0.5
+end
+
+live_loop :hats do
+  sample :drum_cymbal_closed
+  sleep 0.25
+end
