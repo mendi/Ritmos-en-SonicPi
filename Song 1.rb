8276a1ebc2a9c400cce531e3e2639ed02270d933
mendi <vania.unal@gmai.com>
2019-05-08 09:33:54 -0500

    Arreglos de estructura
    Algunos cambios para dejar la estructura de la canción acorde a los
    requerimientos del Sonic Pi.

--- Song 1.rb
+++ Song 1.rb
@@ -74,7 +74,7 @@ sleep 0.5
 # Estrofa 1
 ###########################################
 
-7.times do
+15.times do
   sample :drum_cymbal_closed
   sample :drum_bass_hard
   sleep 0.5
@@ -152,6 +152,7 @@ sample :drum_snare_hard
 sample :drum_cymbal_closed
 sleep 0.5
 
+
 3.times do
   sample :drum_cowbell
   sample :drum_cymbal_closed
@@ -218,12 +219,50 @@ sleep 0.25
 sample :drum_tom_mid_hard
 sleep 0.5
 
+###########################################
+#Intro 3
+###########################################
+
+7.times do
+  sample :drum_cymbal_closed
+  sample :drum_bass_hard
+  sleep 0.5
+  sample :drum_cymbal_closed
+  sleep 0.25
+  sample :drum_snare_soft
+  sleep 0.25
+  sample :drum_cymbal_closed
+  sample :drum_bass_hard
+  sleep 0.5
+  sample :drum_snare_soft
+  sample :drum_cymbal_closed
+  sleep 0.5
+end
+
+sample :drum_tom_hi_soft
+sample :drum_tom_lo_soft
+sleep 0.25
+sample :drum_tom_mid_soft
+sample :drum_bass_hard
+sleep 0.25
+sample :drum_tom_lo_hard
+sample :drum_tom_hi_hard
+sleep 0.25
+sample :drum_tom_mid_soft
+sample :drum_bass_hard
+sleep 0.25
+sample :drum_tom_lo_hard
+sample :drum_tom_hi_hard
+sleep 0.5
+sample :drum_tom_mid_soft
+sample :drum_bass_hard
+sleep 0.5
 
 ###########################################
 # Estrofa 2
 ###########################################
 
-7.times do
+15.times do
   sample :drum_cymbal_closed
   sample :drum_bass_hard
   sleep 0.5
@@ -261,7 +300,7 @@ sleep 0.25
 
 # Rap
 
-7.times do
+15.times do
   sample :drum_bass_soft
   sample :drum_cymbal_closed
   sleep 0.75
@@ -283,6 +322,31 @@ sample :drum_snare_soft
 sleep 0.5
 sample :drum_tom_hi_soft
 sleep 0.25
+
+#################################################
+# Instrumental
+#########################################
+2.times do
+  
+  15.times do
+    sample :bd_zome
+    sleep 0.75
+    sample :sn_dub
+    sleep 0.25
+    sample :bd_zome
+    sleep 0.5
+    sample :sn_dub
+    sleep 0.5
+  end
+  sample :bd_zome
+  sleep 0.75
+  sample :sn_dub
+  sleep 0.25
+  sample :drum_tom_hi_soft
+  sleep 0.5
+  sample :drum_tom_lo_soft
+  sleep 0.5
+end
 
 ###########################################
 # Puente 2
@@ -308,36 +372,20 @@ sleep 0.25
 sample :drum_tom_mid_hard
 sleep 0.5
 
+
 ###########################################
 # Coro
 ###########################################
 
-sample :drum_cowbell
-sample :drum_splash_hard
-sample :drum_heavy_kick
-sleep 0.5
-sample :drum_cymbal_closed
-sleep 0.25
-sample :drum_snare_hard
-sleep 0.25
-sample :drum_cowbell
-sample :drum_cymbal_closed
-sample :drum_bass_hard
-sleep 0.5
-sample :drum_snare_hard
-sample :drum_cymbal_closed
-sleep 0.5
-
-3.times do
+2.times do
   sample :drum_cowbell
-  sample :drum_cymbal_closed
+  sample :drum_splash_hard
   sample :drum_heavy_kick
   sleep 0.5
   sample :drum_cymbal_closed
   sleep 0.25
   sample :drum_snare_hard
   sleep 0.25
-  
   sample :drum_cowbell
   sample :drum_cymbal_closed
   sample :drum_bass_hard
@@ -346,78 +394,70 @@ sleep 0.5
   sample :drum_cymbal_closed
   sleep 0.5
   
-  sample :drum_cowbell
+  3.times do
+    sample :drum_cowbell
+    sample :drum_cymbal_closed
+    sample :drum_heavy_kick
+    sleep 0.5
+    sample :drum_cymbal_closed
+    sleep 0.25
+    sample :drum_snare_hard
+    sleep 0.25
+    
+    sample :drum_cowbell
+    sample :drum_cymbal_closed
+    sample :drum_bass_hard
+    sleep 0.5
+    sample :drum_snare_hard
+    sample :drum_cymbal_closed
+    sleep 0.5
+    
+    sample :drum_cowbell
+    sample :drum_cymbal_closed
+    sample :drum_bass_hard
+    sleep 0.5
+    sample :drum_cymbal_closed
+    sleep 0.25
+    sample :drum_snare_hard
+    sleep 0.25
+    
+    sample :drum_cowbell
+    sample :drum_cymbal_closed
+    sample :drum_bass_hard
+    sleep 0.5
+    sample :drum_snare_hard
+    sample :drum_cymbal_closed
+    sleep 0.25
+    sample :drum_snare_hard
+    sleep 0.25
+  end
+  
+  
   sample :drum_cymbal_closed
   sample :drum_bass_hard
   sleep 0.5
   sample :drum_cymbal_closed
   sleep 0.25
-  sample :drum_snare_hard
+  sample :drum_snare_soft
   sleep 0.25
   
-  sample :drum_cowbell
   sample :drum_cymbal_closed
-  sample :drum_bass_hard
   sleep 0.5
-  sample :drum_snare_hard
   sample :drum_cymbal_closed
+  sample :drum_snare_soft
+  sleep 0.5
+  
+  sample :drum_tom_lo_hard
+  sleep 0.75
+  sample :drum_tom_mid_soft
   sleep 0.25
-  sample :drum_snare_hard
+  
+  sample :drum_tom_hi_hard
+  sample :drum_bass_hard
   sleep 0.25
+  sample :drum_tom_hi_hard
+  sleep 0.25
+  sample :drum_tom_mid_hard
+  sleep 0.5
 end
-
-
-sample :drum_cymbal_closed
-sample :drum_bass_hard
-sleep 0.5
-sample :drum_cymbal_closed
-sleep 0.25
-sample :drum_snare_soft
-sleep 0.25
-
-sample :drum_cymbal_closed
-sleep 0.5
-sample :drum_cymbal_closed
-sample :drum_snare_soft
-sleep 0.5
-
-sample :drum_tom_lo_hard
-sleep 0.75
-sample :drum_tom_mid_soft
-sleep 0.25
-
-sample :drum_tom_hi_hard
-sample :drum_bass_hard
-sleep 0.25
-sample :drum_tom_hi_hard
-sleep 0.25
-sample :drum_tom_mid_hard
-sleep 0.5
-
-#######################################
-# Final
-#######################################
-
-sample :drum_bass_hard
-sample :drum_cymbal_open
-sleep 1
-
-sample :drum_bass_hard
-sample :drum_cymbal_open
-sleep 0.25
-sample :drum_tom_hi_hard
-sleep 0.125
-sample :drum_tom_lo_hard
-sleep 0.125
-sample :drum_bass_hard
-sample :drum_splash_soft
-sleep 0.5
-
-sample :drum_bass_hard
-sample :drum_splash_hard
-sleep 0.75
-sample :drum_snare_soft
-sleep 0.25
-sample :drum_heavy_kick
-sample :drum_splash_hard
 
