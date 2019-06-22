d50863557d7cdb73921c4952b3fe6580dd1b5b21
mendi <vania.unal@gmai.com>
2019-06-22 14:59:14 -0500

    Nueva canción Hh0
    Primer loop

---
+++ Hh0/Beats1.rb
@@ -0,0 +1,109 @@
+use_bpm 12
+
+live_loop :hh0 do
+  
+  sample :bass_dnb_f
+  
+  sample :bd_ada
+  sample :drum_cymbal_closed
+  sleep 0.03125
+  sleep 0.03125
+  
+  
+  sample :bd_ada
+  sleep 0.03125
+  
+  sample :bd_ada
+  sleep 0.03125
+  
+  sample :drum_cymbal_closed
+  sleep 0.03125
+  sleep 0.03125
+  
+  sleep 0.03125
+  sample :bd_ada
+  sleep 0.03125
+  
+  sample :bd_ada
+  sample :drum_cymbal_closed
+  sleep 0.03125
+  sleep 0.03125
+  
+  sleep 0.03125
+  sleep 0.03125
+  
+  sample :sn_dub
+  sample :drum_cymbal_closed
+  sleep 0.03125
+  sleep 0.03125
+  
+  sample :drum_cymbal_closed
+  sleep 0.0078125
+  sample :drum_cymbal_closed
+  sleep 0.0078125
+  sample :drum_cymbal_closed
+  sleep 0.0078125
+  sample :drum_cymbal_closed
+  sleep 0.0078125
+  sample :drum_cymbal_closed
+  
+  sample :bd_ada
+  sleep 0.03125
+  
+  sample :bd_ada
+  sample :drum_cymbal_closed
+  sleep 0.03125
+  sleep 0.03125
+  
+  sleep 0.03125
+  sleep 0.03125
+  
+  
+  sample :drum_cymbal_closed
+  sleep 0.03125
+  sleep 0.03125
+  
+  sleep 0.03125
+  sleep 0.03125
+  
+  sample :bd_ada
+  sample :drum_cymbal_closed
+  sleep 0.03125
+  sample :bd_ada
+  sleep 0.03125
+  
+  sample :bd_ada
+  sleep 0.03125
+  sleep 0.03125
+  
+  sample :sn_dub
+  sample :drum_cymbal_closed
+  sleep 0.03125
+  
+  sample :bd_ada
+  sleep 0.03125
+  
+  sample :bd_ada
+  sample :drum_cymbal_closed
+  sleep 0.0078125
+  sample :drum_cymbal_closed
+  sleep 0.0078125
+  sample :drum_cymbal_closed
+  sleep 0.0078125
+  sample :drum_cymbal_closed
+  sleep 0.0078125
+  
+  
+  sample :drum_cymbal_closed
+  sleep 0.0078125
+  sample :drum_cymbal_closed
+  sleep 0.0078125
+  sample :drum_cymbal_closed
+  sleep 0.0078125
+  sample :drum_cymbal_closed
+  sleep 0.0078125
+  
+  
+  
+  
+end
